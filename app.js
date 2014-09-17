412763ae379968afe8ee696ba3a7cba88241d65a
Niklas Närhinen <niklas@narhinen.net>
2014-09-17 10:01:01 +0300

    Initial support for i18n and l10n

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 var express = require('express'),
     app = express(),
-    nunjucks = require('nunjucks');
+    nunjucks = require('nunjucks'),
+    i18n = require('i18n-abide');
+
+var languages = ['en', 'fi'];
 
 nunjucks.configure('views', {
   autoescape: true,
   express: app
 });
 
+app.use(i18n.abide({
+  supported_languages: languages,
+  default_lang: 'en',
+  debug_lang: 'en',
+  translation_directory: 'i18n'
+}));
+
 app.get('/', function(req, res) {
   res.render('index.html');
+});
+
+app.get('/app/*', function(req, res) {
+  res.render('app.html');
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
