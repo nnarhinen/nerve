08e583a96205c700af2e46e6cf7134972462bb5b
Niklas Närhinen <niklas@narhinen.net>
2014-09-24 17:01:30 +0300

    First steps implementing signup

--- frontend/login.js
+++ frontend/login.js
@@ -1,6 +1,8 @@
 var bean = require('bean'),
     Sizzle = require('sizzle'),
-    ClassList = require('class-list');
+    ClassList = require('class-list'),
+    _ = require('underscore'),
+    axios = require('axios');
 
 var signupContainer = Sizzle('.signup-container')[0],
     loginContainer = Sizzle('.login-container')[0],
@@ -11,4 +13,20 @@ bean.on(document, 'click', '.signup-toggle', function(ev) {
   ev.preventDefault();
   signupContainerClasses.toggle('hide');
   loginContainerClasses.toggle('hide');
+});
+
+var inputAsKeyValuePair = function(el) {
+  return [el.name, el.value];
+};
+
+bean.on(document, 'submit', '.signup-form', function(ev) {
+  ev.preventDefault();
+  var form = ev.currentTarget;
+  var data = _.chain(Sizzle('input[name]', form)).map(inputAsKeyValuePair).object().value();
+  console.log('data', data);
+  axios.post(form.action, data).then(function(resp) {
+    console.log('success', resp);
+  }).catch(function(resp) {
+    console.log('error', resp);
+  });
 });
