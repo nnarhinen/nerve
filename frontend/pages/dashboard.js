e7878e3f5bdd14808d9ac543a36176c098974a84
Niklas Närhinen <niklas@narhinen.net>
2014-09-28 23:55:32 +0300

    Scetch out dashboard

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -8,8 +8,29 @@ var React = require('react'),
 module.exports = React.createClass({
   render: function() {
             return (
-              <div>
+              <div className="container-fluid">
                 <h1>{ i18n.gettext('Dashboard') }</h1>
+                <div className="row">
+                  <div className="col-md-8">What</div>
+                  <div className="col-md-4">
+                    <div className="panel panel-default">
+                      <div className="panel-heading">
+                        <h4 className="panel-title">{ i18n.gettext('Pending invoices') }</h4>
+                      </div>
+                      <div className="panel-body">
+                        <i className="fa fa-spin fa-circle-o-notch"></i>
+                      </div>
+                    </div>
+                    <div className="panel panel-default">
+                      <div className="panel-heading">
+                        <h4 className="panel-title">{ i18n.gettext('E-Mail inbox') }</h4>
+                      </div>
+                      <div className="panel-body">
+                        <i className="fa fa-spin fa-circle-o-notch"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
               );
           }
