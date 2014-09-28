fe6dfc638f406dd0014d21aabb9c9229ca8b4b95
Niklas Närhinen <niklas@narhinen.net>
2014-09-29 00:02:04 +0300

    Add customer-uuid field

--- frontend/pages/settings.js
+++ frontend/pages/settings.js
@@ -18,9 +18,19 @@ module.exports = React.createClass({
               <div>
                 <h1>{ i18n.gettext('Settings') }</h1>
                 <h2>Maventa</h2>
-                <div className="form-group">
-                  <label htmlFor="fe-settings-maventa-api-key">{ i18n.gettext('API-key') }</label>
-                  <input type="text" className="form-control" id="fe-settings-maventa-api-key" name="maventa_api_key" onChange={this.onValueChange} value={this.getStore('settings').maventa_api_key} />
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label htmlFor="fe-settings-maventa-api-key">{ i18n.gettext('API-key') }</label>
+                      <input type="text" className="form-control" id="fe-settings-maventa-api-key" name="maventa_api_key" onChange={this.onValueChange} value={this.getStore('settings').maventa_api_key} />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label htmlFor="fe-settings-maventa-customer-uuid">{ i18n.gettext('Customer UUID') }</label>
+                      <input type="text" className="form-control" id="fe-settings-maventa-customer-uuid" name="maventa_customer_uuid" onChange={this.onValueChange} value={this.getStore('settings').maventa_customer_uuid} />
+                    </div>
+                  </div>
                 </div>
               </div>
               );
