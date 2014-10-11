3c728dd3e5e8375969ca13a8dd3be8dd25114377
Niklas Närhinen <niklas@narhinen.net>
2014-10-11 23:42:25 +0300

    Add datepickers with react-bootstrap-calendar

--- frontend/pages/expense/basic.js
+++ frontend/pages/expense/basic.js
@@ -5,13 +5,44 @@
 var React = require('react'),
     i18n = requirePo('../../../locale/%s/LC_MESSAGES/messages.po'),
     SupplierInput = require('../../components/supplier-input'),
-    InboundInvoiceActions = require('../../actions/inbound-invoice-actions');
+    InboundInvoiceActions = require('../../actions/inbound-invoice-actions'),
+    Input = require('react-bootstrap/Input'),
+    OverlayTrigger = require('react-bootstrap/OverlayTrigger'),
+    Popover = require('react-bootstrap/Popover'),
+    Calendar = require('react-bootstrap-calendar').Calendar,
+    moment = require('moment'),
+    common = require('../../common');
+
+
+var DatePickerInput = React.createClass({
+  getInitialState: function() {
+    return {
+      value: moment(this.props.value),
+      dateFormat: this.props.dateFormat || 'YYYY-MM-DD'
+    };
+  },
+  daySelected: function(m) {
+    this.setState({
+      value: m
+    });
+    this.props.onChange && this.props.onChange(m);
+  },
+  render: function() {
+    return (
+      <OverlayTrigger trigger="click" placement="bottom" overlay={<Popover><Calendar onDaySelected={this.daySelected} selectedDate={this.state.value} /></Popover>}>
+        <Input type="text" onChange={common.noop} name={this.props.name} value={this.state.value.format(this.state.dateFormat)} label={this.props.label} />
+      </OverlayTrigger>
+      );
+  }
+});
+
 
 module.exports = React.createClass({
   onSupplierChanged: function(supplier) {
     InboundInvoiceActions.updateOne(_.extend({}, this.props.expense, {supplier: supplier}));
   },
   render: function() {
+    var dueDate = moment(this.props.expense.due_date);
     return (
       <div className="row">
         <div className="col-md-6">
@@ -19,6 +50,12 @@ module.exports = React.createClass({
             <label htmlFor="fe-expense-supplier">{ i18n.gettext('Supplier') }</label>
             <SupplierInput suppliers={this.props.suppliers} value={this.props.expense.supplier} onSupplierChange={this.onSupplierChanged} />
           </div>
+        </div>
+        <div className="col-md-3">
+          <DatePickerInput onChange={this.expenseDateChanged} label={ i18n.gettext('Invoice date') } value={this.props.expense.expense_date} type="text" />
+        </div>
+        <div className="col-md-3">
+          <DatePickerInput onChange={this.dueDateChanged} label={ i18n.gettext('Due date') } value={dueDate.toString('DD.MM.YYYY')} type="text" />
         </div>
       </div>
       );
