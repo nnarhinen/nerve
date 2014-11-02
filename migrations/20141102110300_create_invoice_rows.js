0e71742bdb2bbc3e71c9b0cef7c21c6327593015
Niklas Närhinen <niklas@narhinen.net>
2014-11-02 17:37:20 +0200

    Transactions does not seem to work here..

--- migrations/20141102110300_create_invoice_rows.js
+++ migrations/20141102110300_create_invoice_rows.js
@@ -1,46 +1,42 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-  return knex.transaction(function(trx) {
-    return trx.schema.createTable('product_lines', function(table) {
+  return knex.schema.createTable('product_lines', function(table) {
+    table.increments('id').primary();
+    table.integer('environment_id').notNull().references('environments.id');
+    table.string('product_line').notNull();
+    table.string('name').notNull();
+
+    table.timestamps();
+    table.unique(['environment_id', 'product_line']);
+  }).then(function() {
+    return knex.schema.createTable('products', function(table) {
       table.increments('id').primary();
       table.integer('environment_id').notNull().references('environments.id');
-      table.string('product_line').notNull();
+      table.integer('product_line_id').references('product_lines.id');
+      table.string('product_number').notNull();
       table.string('name').notNull();
+      table.integer('vat_percent');
+      table.float('unit_price');
+      table.string('unit');
 
       table.timestamps();
-      table.unique(['environment_id', 'product_line']);
-    }).then(function() {
-      return trx.schema.createTable('products', function(table) {
-        table.increments('id').primary();
-        table.integer('environment_id').notNull().references('environments.id');
-        table.integer('product_line_id').references('product_lines.id');
-        table.string('product_number').notNull();
-        table.string('name').notNull();
-        table.integer('vat_percent');
-        table.float('unit_price');
-        table.string('unit');
+      table.unique(['environment_id', 'product_line_id', 'product_number']);
+    });
+  }).then(function() {
+    return knex.schema.createTable('invoice_rows', function(table) {
+      table.increments('id').primary();
+      table.integer('environment_id').notNull().references('environments.id');
+      table.integer('invoice_id').notNull().references('invoices.id');
+      table.integer('product_id').references('products.id');
+      table.string('product_number');
+      table.string('name').notNull();
+      table.integer('vat_percent');
+      table.float('unit_price');
+      table.float('amount');
+      table.string('unit');
 
-        table.timestamps();
-        table.unique(['environment_id', 'product_line_id', 'product_number']);
-      });
-    }).then(function() {
-      return trx.schema.createTable('invoice_rows', function(table) {
-        table.increments('id').primary();
-        table.integer('environment_id').notNull().references('environments.id');
-        table.integer('invoice_id').notNull().references('invoices.id');
-        table.integer('product_id').references('products.id');
-        table.string('product_number');
-        table.string('name').notNull();
-        table.integer('vat_percent');
-        table.float('unit_price');
-        table.float('amount');
-        table.string('unit');
-
-        table.timestamps();
-      });
-    }).then(function() {
-      return trx.commit();
+      table.timestamps();
     });
   });
 };
