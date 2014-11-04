3d0b549af62345539c0853d236beba85c78cc03b
Niklas Närhinen <niklas@narhinen.net>
2014-11-04 20:52:16 +0200

    Use native date input on some touch devices

--- src/frontend/components/date-picker-input.js
+++ src/frontend/components/date-picker-input.js
@@ -8,9 +8,14 @@ var React = require('react'),
 
 var DatePickerInput = module.exports = React.createClass({
   getInitialState: function() {
+    //FIXME
+    var ua = window.navigator.userAgent,
+        isMobileSafari = !!/iPad|iPhone/.exec(ua),
+        isAndroid = !!/Android/.exec(ua);
     return {
       value: this.props.value && moment(this.props.value),
-      dateFormat: this.props.dateFormat || 'yyyy-MM-dd'
+      dateFormat: this.props.dateFormat || 'yyyy-MM-dd',
+      useDateTimePicker: !isAndroid && !isMobileSafari
     };
   },
   daySelected: function(m) {
@@ -22,11 +27,15 @@ var DatePickerInput = module.exports = React.createClass({
   datePickerDateChanged: function(d) {
     this.daySelected(moment(d));
   },
+  dateInputChanged: function(ev) {
+    this.daySelected(moment(ev.target.value));
+  },
   render: function() {
     return (
       <div className="form-group">
         <label className="control-label" htmlFor={this.props.id}>{this.props.label}</label>
-        <DateTimePicker id={this.props.id} time={false} format={this.state.dateFormat} onChange={this.datePickerDateChanged} value={this.state.value.toDate()} />
+        {this.state.useDateTimePicker ? <DateTimePicker id={this.props.id} time={false} format={this.state.dateFormat} onChange={this.datePickerDateChanged} value={this.state.value && this.state.value.toDate()} />
+        : <input className="form-control" type="date" value={this.state.value && this.state.value.format('YYYY-MM-DD')} onChange={this.dateInputChanged} />}
       </div>
     );
   }
