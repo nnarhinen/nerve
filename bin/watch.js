eacef360f8753a76aec855ce6d22fb1aebc87351
Niklas Närhinen <niklas@narhinen.net>
2014-11-04 22:09:47 +0200

    Use yalr for watching

---
+++ bin/watch.js
@@ -0,0 +1,32 @@
+'use strict';
+require('yalr')({
+  path: 'src/backend/public',
+  debounce: 1000,
+  match: [
+    '*.js',
+    '*.css'
+  ]
+});
+
+var browserify = require('browserify'),
+    watchify = require('watchify'),
+    fs = require('fs'),
+    bundler;
+
+require('../src/backend/env');
+
+bundler = watchify(browserify('./src/frontend/index.js', {
+  basedir: __dirname + '/../',
+  debug: true,
+  cache: {},
+  packageCache: {},
+  fullPaths: true
+}));
+
+bundler.transform('reactify');
+bundler.transform('jedify', {lang: 'fi'});
+
+bundler.on('update', function() {
+  bundler.bundle().pipe(fs.createWriteStream(__dirname + '/../src/backend/public/js/application.fi.js'));
+});
+bundler.bundle().pipe(fs.createWriteStream(__dirname + '/../src/backend/public/js/application.fi.js'));
